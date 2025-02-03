fcb31be51ddcf1e389ffc66ce32f8187c1245a08
NoReply Amazon <no-reply@amazon.com>
2025-02-03 03:17:09 +0000

    [Commit] Iteration 1 #errors = 132 <= 137.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/app/Bookstore.Web/Startup.cs][CS0246][4,12] The type or namespace name 'OwinStartupAttribute' could not be found (are you missing a using directive or an assembly reference?)``.

--- app/Bookstore.Web/Startup.cs
+++ app/Bookstore.Web/Startup.cs
@@ -1,21 +1,50 @@
-using Microsoft.AspNetCore.Owin;
-
-
-[assembly: OwinStartup(typeof(Bookstore.Web.Startup))]
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Bookstore.Web
 {
     public class Startup
     {
-        public void Configuration(IAppBuilder app)
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Configure services here
+        }
+
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
             LoggingSetup.ConfigureLogging();
 
             ConfigurationSetup.ConfigureConfiguration();
 
-            DependencyInjectionSetup.ConfigureDependencyInjection(app);
+            // Note: DependencyInjection should be configured in ConfigureServices method
+            // DependencyInjectionSetup.ConfigureDependencyInjection(app);
 
-            AuthenticationConfig.ConfigureAuthentication(app);
+            // Authentication configuration might need to be adjusted for ASP.NET Core
+            // AuthenticationConfig.ConfigureAuthentication(app);
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
